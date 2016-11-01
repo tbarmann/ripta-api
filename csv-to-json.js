68e9ffeff622a6fda8d7d71f0e2c37b1aff48308
Timothy Barmann <tbarmann92@gmail.com>
2016-11-01 18:59:01 -0400

    converter for stops.csv to stops.json

---
+++ csv-to-json.js
@@ -0,0 +1,16 @@
+"use strict";
+const parse = require('csv-parse');
+
+const fs = require('fs');
+
+const parser = parse({delimiter: ',', columns: true}, function(err, data){
+  fs.writeFile('stops.json', JSON.stringify(data, null, 2), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('finished');
+
+  });
+});
+
+fs.createReadStream(__dirname+'/static/google_transit/stops.csv').pipe(parser);
