bc073eda0c563edcd73a2177fdf6635c30d9a56b
Timothy Barmann <tbarmann92@gmail.com>
2016-11-03 18:59:31 -0400

    Add Elias work on cleaning up stop addresses

--- static/google_transit/clean-stops.js
+++ static/google_transit/clean-stops.js
@@ -30,6 +30,9 @@ const abbrevOrCapitalize = (w) => {
   return (replace.hasOwnProperty(w)) ? replace[w] : _.capitalize(w)
 }
 
+// stops = JSON.parse(File.read('stops.json'))
+
+
 // # sub in abbreviated values
 // # fix words that start with '(', "(stop" => "(Stop"
 // # fix words with '&' in the middle, "stop&shop" => "Stop&Shop"
@@ -46,3 +49,16 @@ const abbrevOrCapitalize = (w) => {
 //   end
 //   w
 // end
+
+// # shorten exact addresses "S Angell at 120 S Angell" => "120 S Angell"
+// def shorten_exact_address(w)
+//   format = /^(?<street_1>.+) at (?<addr>\d+) (?<street_2>.+?)( St)?( Rd)?$/ =~ w
+//   (format && street_1 == street_2) ? w.split(' at ')[1] : w
+// end
+
+// stops.each do |s|
+//   desc = s["stop_name"].downcase.split(' ').map{|w| process_word(w)}.join(' ')
+//   s["stop_desc"] = shorten_exact_address(desc)
+// end
+
+// File.write('NEW_stops.json', JSON.pretty_generate(stops))
