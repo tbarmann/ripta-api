d629fbf0bd48318113cb02362edc9f9d5be58d81
Timothy Barmann <tbarmann92@gmail.com>
2016-11-04 20:02:06 -0400

    Separate filtering code into its own module

---
+++ filters.js
@@ -0,0 +1,52 @@
+const routes = require('./static/routes.json');
+const trips = require('./static/trips.json');
+const _ = require('lodash');
+
+const routesIndexed = _.keyBy(routes, 'route_id');
+const tripsIndexed = _.keyBy(trips, 'trip_id');
+
+
+const validApiTypes = ['tripupdates', 'vehiclepositions', 'servicealerts'];
+const validDirectionTypes = ['inbound', 'outbound'];
+
+const typeToKey = (type) => {
+  return {
+    tripupdates: 'trip_update',
+    vehiclepositions: 'vehicle',
+    servicealerts: 'alert'
+  }[type];
+}
+
+const isValidRouteId = (routeId) => {
+  return routesIndexed.hasOwnProperty(routeId);
+}
+
+const routeToRouteId = (route) => {
+  return route.toLowerCase().replace('x','');
+}
+
+const filterByRoute = (data, type, route) => {
+  const routeId = routeToRouteId(route);
+  if (!isValidRouteId(routeId)) {
+    return data;
+  }
+  const filtered = _.filter(data.entity, (record) => {
+    return (record[typeToKey(type)].trip.route_id === routeId);
+  });
+  return { header: data.header, entity: filtered };
+}
+
+const filterByDirection = (data, type, direction) => {
+  direction = direction.toLowerCase();
+  if (!_.includes(validDirectionTypes, direction)) {
+    return data;
+  }
+  const direction_id = (direction === 'outbound') ? '1' : '0';
+  const filtered = _.filter(data.entity, (record) => {
+    const trip_id = record[typeToKey(type)].trip.trip_id;
+    return (direction_id === tripsIndexed[trip_id].direction_id);
+  });
+  return { header: data.header, entity: filtered };
+}
+
+module.exports = { filterByRoute, filterByDirection }
--- index.js
+++ index.js
@@ -6,21 +6,18 @@ const jsonfile = require('jsonfile');
 const moment = require('moment-timezone');
 const path = require('path');
 const _ = require('lodash');
-const routes = require('./static/routes.json');
-const trips = require('./static/trips.json');
+const filterByRoute = require('./filters').filterByRoute;
+const filterByDirection = require('./filters').filterByDirection;
 
 const port = 3000;
 
 //const riptaApiBaseUrl = 'http://realtime.ripta.com:81/api/';
 const riptaApiBaseUrl = 'http://localhost:3000/static/';
-const validApiTypes = ['tripupdates', 'vehiclepositions', 'servicealerts'];
-const validDirectionTypes = ['inbound', 'outbound'];
 
 const app = express();
 const staticOptions = { index: 'index.htm' };
 
-let tripsIndexed = {};
-let routesIndexed = {};
+const validApiTypes = ['tripupdates', 'vehiclepositions', 'servicealerts'];
 
 const fetchBaseApi = (type, callback) => {
   const riptaApiUrl = `${riptaApiBaseUrl}${type}?format=json`;
@@ -30,46 +27,6 @@ const fetchBaseApi = (type, callback) => {
       callback(JSON.parse(data));
     }
   });
-}
-
-const typeToKey = (type) => {
-  return {
-    tripupdates: 'trip_update',
-    vehiclepositions: 'vehicle',
-    servicealerts: 'alert'
-  }[type];
-}
-
-const filterByDirection = (data, type, direction) => {
-  direction = direction.toLowerCase();
-  if (!_.includes(validDirectionTypes, direction)) {
-    return data;
-  }
-  const direction_id = (direction === 'outbound') ? '1' : '0';
-  const filtered = _.filter(data.entity, (record) => {
-    const trip_id = record[typeToKey(type)].trip.trip_id;
-    return (direction_id === tripsIndexed[trip_id].direction_id);
-  });
-  return { header: data.header, entity: filtered };
-}
-
-const isValidRouteId = (routeId) => {
-  return routesIndexed.hasOwnProperty(routeId);
-}
-
-const routeToRouteId = (route) => {
-  return route.toLowerCase().replace('x','');
-}
-
-const filterByRoute = (data, type, route) => {
-  const routeId = routeToRouteId(route);
-  if (!isValidRouteId(routeId)) {
-    return data;
-  }
-  const filtered = _.filter(data.entity, (record) => {
-    return (record[typeToKey(type)].trip.route_id === routeId);
-  });
-  return { header: data.header, entity: filtered };
 }
 
 app.use(express.static('public', staticOptions));
@@ -122,13 +79,9 @@ const startServer = () => {
   });
 }
 
-const initialize = () => {
-  routesIndexed = _.keyBy(routes, 'route_id');
-  tripsIndexed = _.keyBy(trips, 'trip_id');
-  startServer();
-}
+startServer();
 
-initialize();
+
 
 
 
