bf455d69240135878b94b6338e8afcce55c5b24f
Timothy Barmann <tbarmann92@gmail.com>
2017-06-27 18:01:41 -0400

    Hardcode files in csv-to-json, add options

--- csv-to-json.js
+++ csv-to-json.js
@@ -1,22 +1,33 @@
 "use strict";
-const parse = require('csv-parse');
+const csvParse = require('csv-parse');
 
 const fs = require('fs');
 
-const csvFile = 'routes.csv';
+const parse = (csvFile) => {
 
-const fileBase = csvFile.split('.')[0];
-const readPath = __dirname+'/static/google_transit/';
-const writePath = __dirname+'/static/';
+  const fileBase = csvFile.split('.')[0];
+  const readPath = __dirname+'/static/google_transit/';
+  const writePath = __dirname+'/static/';
+  const options = {
+    delimiter: ',',
+    columns: true,
+    auto_parse: true,
+    trim: true
+  };
 
-const parser = parse({delimiter: ',', columns: true}, function(err, data){
-  fs.writeFile(writePath + fileBase + '.json', JSON.stringify(data, null, 2), function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('finished');
+  const parser = csvParse(options, function(err, data){
+    fs.writeFile(writePath + fileBase + '.json', JSON.stringify(data, null, 2), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('finished writing: ' + writePath+fileBase);
 
+    });
   });
-});
+  fs.createReadStream(readPath + csvFile).pipe(parser);
+}
 
-fs.createReadStream(readPath + csvFile).pipe(parser);
+const files = ['routes.txt', 'stops.txt', 'trips.txt'];
+
+files.map((file)=>{ parse(file);});
+
