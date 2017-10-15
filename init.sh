f04c0d95824db071ce912e0cc3aaf22575f53dd5
Timothy Barmann <tbarmann92@gmail.com>
2017-10-15 14:24:52 -0400

    Add init script with db seeding

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+URL="https://www.ripta.com/stuff/contentmgr/files/0/3cda81dfa140edbe9aae214b26245b4a/files/google_transit.zip"
+echo 'Downloading...'
+cd ./static/google_transit
+rm *.txt
+curl -sS "${URL}" > file.zip && \
+unzip file.zip
+rm file.zip
+psql ripta -f seed.sql
+
+cd ../..
+node csv-to-json.js
+
+
+
+
