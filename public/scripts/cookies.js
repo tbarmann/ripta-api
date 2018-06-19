0647978acee7c1fde49c0d6883b374717f5ec90e
Timothy Barmann <tbarmann92@gmail.com>
2018-06-19 18:18:11 -0400

    fix lint errors in cookies.js

--- public/scripts/cookies.js
+++ public/scripts/cookies.js
@@ -1,24 +1,33 @@
-function createCookie(name,value,days) {
-	if (days) {
-		var date = new Date();
-		date.setTime(date.getTime()+(days*24*60*60*1000));
-		var expires = "; expires="+date.toGMTString();
-	}
-	else var expires = "";
-	document.cookie = name+"="+value+expires+"; path=/";
+/* exported createCookie */
+function createCookie(name, value, days) {
+  var expires;
+  var date = new Date();
+  if (days) {
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toGMTString();
+  } else {
+    expires = '';
+  }
+  document.cookie = name + '=' + value + expires + '; path=/';
 }
 
+/* exported readCookie */
 function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-		var c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
+  var i;
+  var c;
+  var nameEQ = name + '=';
+  var ca = document.cookie.split(';');
+  for (i = 0; i < ca.length; i += 1) {
+    c = ca[i];
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1, c.length);
+    }
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 
+/* exported eraseCookie */
 function eraseCookie(name) {
-	createCookie(name,"",-1);
+  createCookie(name, '', -1);
 }
